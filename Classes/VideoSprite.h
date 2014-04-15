ce4ba0a4b947ade8a3b798ad6139bc9276f2c3f0
andyque <guanghui8827@126.com>
2014-04-15 10:09:04 +0800

    add a skeleton

--- Classes/VideoSprite.h
+++ Classes/VideoSprite.h
@@ -25,6 +25,17 @@ using cocos2d::SpriteBatchNode;
 class VideoSprite : public SpriteBatchNode
 {
 public:
+    ~VideoSprite();
+    
+    VideoSprite* createWithVideoFile(const std::string& videoFileName);
+    bool initWithVideoFile(const std::string& videoFileName);
+    void updateTexture();
+    
+    
+    
+private:
+    VideoSprite();
+    CC_DISALLOW_COPY_AND_ASSIGN(VideoSprite);
     AVURLAsset *asset;
     AVAssetTrack *videoTrack;
     AVAssetReader *assetReader;
--- Classes/VideoSprite.mm
+++ Classes/VideoSprite.mm
@@ -7,3 +7,38 @@
 //
 
 #include "VideoSprite.h"
+
+
+VideoSprite::VideoSprite()
+{
+    
+}
+
+VideoSprite::~VideoSprite()
+{
+    
+}
+
+
+VideoSprite* VideoSprite::createWithVideoFile(const std::string &videoFileName)
+{
+    VideoSprite* sprite = new VideoSprite;
+    if (sprite && sprite->initWithVideoFile(videoFileName)) {
+        sprite->autorelease();
+        return sprite;
+    }
+    CC_SAFE_DELETE(sprite);
+    return sprite;
+}
+
+bool VideoSprite::initWithVideoFile(const std::string &videoFileName)
+{
+    bool ret = true;
+    do {
+        if (!SpriteBatchNode::init()) {
+            ret = false;
+            break;
+        }
+    } while (0);
+    return ret;
+}
